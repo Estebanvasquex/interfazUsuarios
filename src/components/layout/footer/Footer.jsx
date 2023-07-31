7d70bca3c69232ed37efff7ca3a43490ab01c6eb
estebanvasquex <estebanvasquex@gmail.com>
2023-07-31 13:35:43 -0500

    Se agrea un pequeño cambio en el modal

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <footer>
         <h5>
           Creado por{" "}
-          <a href="https://github.com/Estebanvasquex">estebanVásquez</a> Prueba
+          <a href="#">Esteban Vásquez</a> Prueba
           HYG
         </h5>
       </footer>
